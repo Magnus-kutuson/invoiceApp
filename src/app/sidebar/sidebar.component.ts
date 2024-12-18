b9415dfd45f027b1c54f3cbbf1f2ac292dfdcb9d
Magnus-kutuson <magnus.kutuson@amalitech.com>
2024-12-18 15:44:22 +0000

    done with the sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ThemeChangerComponent } from '../theme-changer/theme-changer.component';
+
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [ThemeChangerComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
