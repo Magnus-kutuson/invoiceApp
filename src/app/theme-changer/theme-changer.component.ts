fe6e4a8492a4669974b778acf3679582238b4211
Magnus-kutuson <magnus.kutuson@amalitech.com>
2024-12-18 15:13:13 +0000

    generated a new component: theme-changer

---
+++ src/app/theme-changer/theme-changer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-changer',
+  standalone: true,
+  imports: [],
+  templateUrl: './theme-changer.component.html',
+  styleUrl: './theme-changer.component.css'
+})
+export class ThemeChangerComponent {
+
+}
