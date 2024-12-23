3c8949e2ffebbbe41bb2914fdfddfbcb413582c3
Magnus-kutuson <magnus.kutuson@amalitech.com>
2024-12-23 14:06:59 +0000

    Finished some of the features in the header component

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { ThemeChangerComponent } from '../theme-changer/theme-changer.component';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+
 
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [ThemeChangerComponent],
+  imports: [ThemeChangerComponent, RouterOutlet, HeaderComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
