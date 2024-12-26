9f5031a1f9777a7b9a6db82ebb8e196ad42cc6ea
Magnus-kutuson <magnus.kutuson@amalitech.com>
2024-12-26 10:36:55 +0000

    finished the icons component and generated a new component: avatar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { ThemeChangerComponent } from '../theme-changer/theme-changer.component';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
+import { IconsComponent } from '../icons/icons.component';
 
 
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [ThemeChangerComponent, RouterOutlet, HeaderComponent],
+  imports: [ThemeChangerComponent, RouterOutlet, HeaderComponent, IconsComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
