b26015dfcd7fac654d1274bf111e7d3b3ee0fbbe
Magnus-kutuson <magnus.kutuson@amalitech.com>
2024-12-26 10:30:24 +0000

    generated an icons component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  filters = [
+    {id: '1', title:'Draft'},
+    {id: '2', title:'Pending'},
+    {id: '3', title:'Paid'},
+  ]
 
 }
