6197fab13d01e5338bf6b019010f75e854b10b90
Magnus-kutuson <magnus.kutuson@amalitech.com>
2024-12-29 11:32:36 +0000

    Finished headline component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { BadgeComponent } from '../badge/badge.component';
+import { HeadlineComponent } from '../headline/headline.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule,BadgeComponent],
+  imports: [CommonModule,BadgeComponent, HeadlineComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
--- src/app/headline/headline.component.ts
+++ src/app/headline/headline.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-headline',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './headline.component.html',
   styleUrl: './headline.component.css'
 })
 export class HeadlineComponent {
-
+@Input() type: 'H1' | 'H2' | 'H3' | 'H4'  = 'H1';
 }
