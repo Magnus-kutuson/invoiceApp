ab0ec696e585209669aaeca91f967ee5ce7bb46b
Magnus-kutuson <magnus.kutuson@amalitech.com>
2024-12-30 08:42:22 +0000

    generated a layout component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { BadgeComponent } from '../badge/badge.component';
 import { HeadlineComponent } from '../headline/headline.component';
+import { TextComponent } from '../text/text.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule,BadgeComponent, HeadlineComponent],
+  imports: [CommonModule,BadgeComponent, HeadlineComponent, TextComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
