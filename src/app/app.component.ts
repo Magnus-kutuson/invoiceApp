f313e171638b36a8504a64cf688d0fc5c5157bdc
Magnus-kutuson <magnus.kutuson@amalitech.com>
2025-01-06 16:12:19 +0000

    Add date picker component and integrate into app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import  { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
-import { FormsComponent } from './forms/forms.component';
+import { DatePickerComponent } from './date-picker/date-picker.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, LayoutComponent, FormsComponent],
+  imports: [RouterOutlet, CommonModule, LayoutComponent, DatePickerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
