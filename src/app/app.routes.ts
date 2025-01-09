81ea5f116d40cda87d35b72e7aa8a658ce919042
Magnus-kutuson <magnus.kutuson@amalitech.com>
2025-01-09 19:38:01 +0000

    Add login component with form validation and styles; update app routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { InvoiceComponent } from './invoice/invoice.component';
 import { SelectedInvoiceComponent } from './selected-invoice/selected-invoice.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   {
     path: '',
     component: LayoutComponent,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form Submitted!', this.loginForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
