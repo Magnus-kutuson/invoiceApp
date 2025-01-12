6c668f0b6a9cf3b12550b4a0e831fed961f18697
Magnus-kutuson <magnus.kutuson@amalitech.com>
2025-01-12 17:06:44 +0000

    Remove login component and associated files; update app routes and imports accordingly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import  { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { DeleteCardComponent } from './delete-card/delete-card.component';
-import { LoginComponent } from './login/login.component';
 import { DropdownComponent } from './dropdown/dropdown.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, LayoutComponent,DeleteCardComponent, LoginComponent, DropdownComponent],
+  imports: [RouterOutlet, CommonModule, LayoutComponent,DeleteCardComponent, DropdownComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
